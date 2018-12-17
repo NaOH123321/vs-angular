51788770cba5624f6b339a646d4824aa656c2175
NaOH <duanhaixiao@hotmail.com>
2018-12-17 17:41:45 +0800

    commit -m

--- Datas/Data.cs
+++ Datas/Data.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using vstaskmgr.Model;
 
 namespace vstaskmgr.Datas
 {
@@ -13,14 +14,14 @@ namespace vstaskmgr.Datas
             var document1 = new BsonDocument
             {
                 { "Cn",   "我突然就觉得自己像个华丽的木偶,演尽了所有的悲欢离合,可是背上总是有无数闪亮的银色丝线,操纵我哪怕一举手一投足。" },
-                { "En",    "I suddenly feel myself like a doll,acting all kinds of joys and sorrows.There are lots of shining silvery thread on my back,controlling all my action."},
+                { "En",    "I suddenly feel myself like a doll,acting all kinds of joys and sorrows.There are lots of shining silvery thread on my back,controlling all my action." },
                 { "Pic",  "/assets/img/quotes/0.jpg" }
             };
 
             var document2 = new BsonDocument
             {
                 { "Cn",    "被击垮通常只是暂时的，但如果你放弃的话，就会使它成为永恒。（Marilyn vos Savant）" },
-                { "En",  "Being defeated is often a temporary condition. Giving up is what makes it permanent."},
+                { "En",  "Being defeated is often a temporary condition. Giving up is what makes it permanent." },
                 { "Pic",  "/assets/img/quotes/1.jpg" }
             };
 
@@ -28,10 +29,28 @@ namespace vstaskmgr.Datas
             var document3 = new BsonDocument
             {
                 { "Cn",  "不要只因一次挫败，就放弃你原来决心想达到的梦想。（莎士比亚）" },
-                { "En",   "Do not, for one repulse, forgo the purpose that you resolved to effect."},
+                { "En",   "Do not, for one repulse, forgo the purpose that you resolved to effect." },
                 { "Pic",  "/assets/img/quotes/2.jpg" }
             };
         }
 
+        private void Project()
+        {
+            var project1 = new BsonDocument
+            {
+                { "Name",   "企业协作平台" },
+                { "Desc",    "这是一个企业内部项目" },
+                { "CoverImg",  "assets/img/covers/0.jpg" },
+                { "Members", new BsonArray{"BkkDvwee-", "3", "4"} }
+            };
+
+            var project2 = new BsonDocument
+            {
+                { "Name",   "自动化测试项目" },
+                { "Desc",    "这是一个企业内部项目"},
+                { "CoverImg",  "assets/img/covers/1.jpg" },
+                { "Members", new BsonArray{ "BkkDvwee-", "4", "37489e0c-df34-c261-71c4-ce75357e3035" } }
+            };
+        }
     }
 }
